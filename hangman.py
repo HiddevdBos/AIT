ad6ccf653837c7e04faf16e273e9b90c4bffd7c0
hiddevdbos <hiddevandenbos@upcmail.nl>
2020-06-12 21:39:08 +0200

    changed from 1 to 2 player game

--- hangman.py
+++ hangman.py
@@ -1,21 +1,54 @@
-from host import *
 from player import *
 
-player = Player()	# player trying to guess the word
-host = Host() 		# host of the game
+# start of the game, player chooses a word and 
+def startGame(player_1, player_2):
+	player_1.chooseWord(len(player_2.result()))	# choose a random word
+	player_2.getAnswer(player_1.giveAnswer())	# tell player_1 how many letters the word is
+	player_2.wordLength()	# process word length 
 
-host.chooseWord()	# choose a random word
-player.getAnswer(host.getAnswer())	# tell player how many letters the word is
-player.wordLength()	# process word length 
+	print(player_1.name(), "chooses the word", player_1.result())
 
-while player.notFinished() and host.incorrectGuesses() < 8:
-	player.guess()	# guess a letter
-	host.updateAnswer(player.currentGuess()) # show whether letter is present
-	player.getAnswer(host.getAnswer())	# process hosts reaction
-	player.remaining()
+def playRound(player_1, player_2):
+	player_1.guess()	# guess a letter
 
-	print(player.currentGuess()) # print guess of the player
-	print(host.getAnswer())			 # print letters of word known by player
-	print(player.possibilities())
-	print(host.incorrectGuesses())
-	print('\n')
+	print(player_1.name(), "guesses", player_1.currentGuess(), '\n')
+
+	player_2.processGuess(player_1.currentGuess())
+	player_1.updateAnswer(player_2.currentGuess()) # show whether letter is present
+	player_2.updateAnswer(player_1.currentGuess()) # show whether letter is present
+	player_1.getAnswer(player_2.giveAnswer())	# process other player's reaction
+	player_2.getAnswer(player_1.giveAnswer())	# process other player's reaction
+	player_1.remaining()
+	player_2.remaining()
+
+	print(player_1.name(), 'current knowledge is',''.join(player_2.giveAnswer()))			 # print letters of word known by player_1
+	print(player_1.name(), "has", player_1.possibilities(), "possible worlds remaining \n")
+	print(player_2.name(), 'current knowledge is',''.join(player_1.giveAnswer()))			 # print letters of word known by player_1
+	print(player_2.name(), "has", player_2.possibilities(), "possible worlds remaining \n")
+
+# initialize 2 players
+player_1 = Player('player 1')		# player_1 trying to guess the word
+player_2 = Player('player 2') 	# player_2 of the game
+
+# prepare the game
+startGame(player_1, player_2)
+startGame(player_2, player_1)
+input("\npress any key to start the game\n")
+
+# play the game. Continues until one of the players guesses the word of the other player
+while True:
+	if player_1.possibilities() == 1:	# if player_1 knows player_2's word
+		print (player_1.name(), "guesses", player_1.guessAnswer())
+		print (player_1.name(), "wins")
+		break
+	
+	playRound(player_1, player_2)
+	input("press any key to go to the next round\n")
+	
+	if player_2.possibilities() == 1: # if player_2 knows player_1's word
+		print (player_2.name(), "guesses", player_2.guessAnswer())
+		print (player_2.name(), "wins")	
+		break
+	
+	playRound(player_2, player_1)
+	input("press any key to go to the next round\n")
--- player.py
+++ player.py
@@ -1,52 +1,105 @@
 from nltk.corpus import words
+import random
 
 class Player:
 	
-	def __init__(self):
+	def __init__(self, name):
+		self.__result = ''  					# word to be found
+		self.answer_opponent = []     # (part of) answer
 		self.previous_guesses = []    # guesses made
-		self.answer = []     # (part of) answer
-		self.possible = []	 # all possible answers
-		self.current_guess = ''
+		self.answer_to_guess = []     # (part of) answer
+		self.possible = []	 					# all possible answers
+		self.current_guess = ''				# current letter guessed
+		self.__name = name 						# name of the player
+
+	#choose the word the other player has to guess
+	def chooseWord(self, length):
+		if length == 0:
+			self.__result = random.choice(words.words()).lower()	# choose random word
+		else:
+			while len(self.__result) != length:
+				self.__result = random.choice(words.words()).lower()	# choose random word
+		self.answer_opponent = ['.'] * len(self.__result)						# information known by player
 
 
+	#return the result
+	def result(self):
+		return self.__result
+
+	# update 
+	def updateAnswer(self, guess):
+		correct = 0
+		for idx, item in enumerate(self.__result):		#  update answer
+			if item == guess:
+				correct = 1
+				self.answer_opponent[idx] = guess
+
+	# guess word of other player (when certain)
+	def guessAnswer(self):
+		return self.possible[0]
+
+	# select all possible worlds, based on the length of the word
 	def wordLength(self):
 		for word in words.words():								
-			if len(word) == len(self.answer):						
+			if len(word) == len(self.answer_to_guess):
 				self.possible.append(word.lower())
 
+	# make the guess that reduces the amount of possible worlds the most
 	def guess(self):
-		counts = dict()														# count of all letters 
+		count = dict()														# count of all letters 
 		for word in self.possible:
+			current_word_count = dict()		# count a letter only once per world
 			for letter in word:
-				if letter in counts:
-					counts[letter] += 1
+				if letter in count:
+					if letter not in current_word_count:
+						count[letter] += 1									
+						current_word_count[letter] = 1					
 				elif letter not in self.previous_guesses:
-					counts[letter] = 1
-		self.current_guess = max(counts, key=counts.get)		# guess the letter that occurs most often
+					count[letter] = 1
+		self.current_guess = max(count, key=count.get)		# guess the letter that occurs most often
 		self.previous_guesses.append(self.current_guess)
 
+	# process guess of other player
+	def processGuess(self, guess):
+		self.current_guess = guess
+		self.previous_guesses.append(self.current_guess)
+
+
+	# determine which worlds are still possible
 	def remaining(self):
 		store_possible = self.possible.copy()
 		self.possible.clear()
-		for word in store_possible:
+		for word in store_possible:				# for all possible worlds
 			correct = 1
 			count = 0
-			for letter in word:
-				if self.answer[count] != '.' and self.answer[count] != letter:
+			for letter in word:							# for every letter in the word
+				# if the word contains a letter which has been guessed an is not at that position
+				if self.answer_to_guess[count] != '.' and self.answer_to_guess[count] != letter:
+					correct = 0
+				# if word contains a letter which has already been guessed and shown to be incorrect
+				if letter in self.previous_guesses and self.answer_to_guess[count] == '.' :
 					correct = 0
 				count += 1
 			if correct == 1:
 				self.possible.append(word)
 
+	# process announcement of other player
 	def getAnswer(self, updated_answer):
-		self.answer.clear()
-		self.answer = updated_answer.copy()
+		self.answer_to_guess.clear()
+		self.answer_to_guess = updated_answer.copy()
 
+	# return current guessed letter
 	def currentGuess(self):
 		return self.current_guess
 
-	def notFinished(self):
-		return '.' in self.answer 
-
+	# return amount of possible worlds left
 	def possibilities(self):
 		return len(self.possible)
+
+  # tell other player which guessed letters are at which positions
+	def giveAnswer(self):
+		return self.answer_opponent
+
+	# return name
+	def name(self):
+		return self.__name
